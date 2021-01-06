1e35b3054ae9b289a59526774d0b339115f0fbbc
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-VKM0MRH>
2021-01-06 11:45:08 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Jibaki</title>
+        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col alert-danger">
+                    1 de 2
+                </div>
+
+                <div class="col alert-primary">
+                    2 de 2
+                </div>
+
+            </div>
+            <div class="row alert-info text-center my-1">
+                <div class="col alert-danger">1 - 3</div>
+                <div class="col alert-primary">2 - 3</div>
+                <div class="col alert-success">3 - 3</div>
+
+
+            </div>
+        </div>
+
+        <div>
+            <button class="btn btn-outline-success btn-lg btn-block my-1">Cadastrar</button>
+        </div>
+
+        <div class="btn btn-outline-secondary btn-lg btn-block">Editar</div>
+
+        <div>
+            <button class="btn btn-outline-danger btn-lg btn-block my-1">Excluir</button>
+        </div>
+        <div>
+
+        </div>
+
+        <!--div.row>div.col*4-->
+        <!--div.row>(div.col>img)+div.col>h1+p+btn.btn.btn-danger-->
+
+        <div class="row">
+            <div class="col">
+                <img src="https://place-hold.it/230x300" alt=""/>
+            </div>
+            <div class="col">
+                <h1>Teste</h1>
+                <p>Testando esta bagaça...</p>
+                <button class="btn btn-outline-success">Testando...</button>
+            </div>
+        </div>
+
+
+
+
+
+
+    </body>
+</html>
